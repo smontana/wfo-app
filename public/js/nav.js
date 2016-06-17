5781f7b01a80ffac502a220c1e27e2ab52878157
Stephen Montana <stephen.a.montana@gmail.com>
2016-06-17 14:45:29 -0500

    app navigation almost done

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,25 +1,65 @@
-function findAndOpenMatchingSection(btn_id) {
-  // hideAllSections();
-  var sections = document.querySelectorAll(".container section");
+const storage = require('electron-json-storage')
 
-  var selected_section_id = btn_id.id += '_sec';
-  var selected_section = document.getElementById(selected_section_id);
-  // selected_section.classList.add('is-shown');
-  // console.log(selected_section);
- 
-  Array.prototype.forEach.call(sections, function (section) { 
+// Default to the view that was active the last time the app was open
+storage.get('activeSectionButtonId', function (err, id) {
+  if (err) return console.error(err)
 
-    if (selected_section_id == section.id) {
-      section.classList.add('is-shown');
-    } else {
-      section.classList.remove('is-shown');
-    }
-  });
+  if (id && id.length) {
+    const section = document.getElementById(id)
+    if (section) section.click()
+  } else {
+    activateDefaultSection()
+  }
+})
+
+document.body.addEventListener('click', function (event) {
+  console.log(event)
+  const selected_section_id = event.target.id
+
+  if (event.target.nodeName == 'BUTTON') {
+    handleSectionTrigger(event, selected_section_id)
+  }
+})
+
+function handleSectionTrigger(event, open_section_id) {
+  hideAllSectionsAndDeselectButtons(open_section_id)
+
+  event.target.classList.add('is-selected')
+
+  const section_id = event.target.getAttribute('id') + 'sec'
+
+  console.log(section_id)
+
+  document.getElementById(section_id).classList.add('is-shown')
+
+  // Save currently active button in localStorage
+  const buttonId = event.target.getAttribute('id')
+  storage.set('activeSectionButtonId', buttonId, function (err) {
+    if (err) return console.error(err)
+  })
 }
 
-function hideAllSections () {
-  const sects = document.querySelectorAll('.container section .is-shown');
-  Array.prototype.forEach.call(sects, function (section) {
-    section.classList.remove('is-shown');
-  });
+function activateDefaultSection () {
+  document.getElementById('homepage_').click()
+}
+
+function hideAllSectionsAndDeselectButtons (open_section_id) {
+  const leave_open_section_id = open_section_id + 'sec'
+  const sections = document.querySelectorAll('.app-section')
+  Array.prototype.forEach.call(sections, function (section) {
+    const this_id = section.getAttribute('id')
+
+    if (this_id !== leave_open_section_id) {
+      section.classList.remove('is-shown')
+    }
+  })
+
+  const buttons = document.querySelectorAll('button')
+  Array.prototype.forEach.call(buttons, function (button) {
+    const this_btn_id = button.getAttribute('id')
+    
+    if (this_btn_id !== open_section_id) {
+      button.classList.remove('is-selected')
+    }
+  })
 }
