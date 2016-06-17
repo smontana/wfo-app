2946c14b181a8c5c87897c7af4dd55e085b09cd8
Stephen Montana <stephen.a.montana@gmail.com>
2016-06-16 19:00:37 -0500

    added last window state support

--- main.js
+++ main.js
@@ -1,31 +1,74 @@
 'use strict';
+const path = require('path');
+const fs = require('fs');
+const electron = require('electron');
+const app = electron.app;
+const config = require('./lib/config');
 
-const {app, BrowserWindow} = require('electron');
-const locals = {/* ...*/};
+// jade stuff
+const locals = {};
 const jade = require('./lib/electron-jade')({pretty: true}, locals);
 
 require('dotenv').load();
 
 let mainWindow;
+let mainWindowBounds;
+let isQuitting = false;
+
+function createMainWindow() {
+  const lastWindowState = config.get('lastWindowState');
+
+  const win = new electron.BrowserWindow({
+    show: false,
+    x: lastWindowState.x,
+    y: lastWindowState.y,
+    width: lastWindowState.width,
+    height: lastWindowState.height,
+    minWidth: 800,
+    minHeight: 600,
+    titleBarStyle: 'hidden-inset',
+    autoHideMenuBar: true
+  });
+
+  // NOTE: the URL is enclosed in backticks NOT single quotes
+  win.loadURL(`file://${__dirname}/index.jade`);
+
+  return win;
+}
+
+app.on('ready', function() {
+  mainWindow = createMainWindow();
+
+  const page = mainWindow.webContents;
+
+  page.on('dom-ready', function() {
+    page.insertCSS(fs.readFileSync(path.join(__dirname+'/public/stylesheets/photon-kit/css/', 'photon.css'), 'utf8'));
+    page.insertCSS(fs.readFileSync(path.join(__dirname+'/public/stylesheets/', 'c3.min.css'), 'utf8'));
+    page.insertCSS(fs.readFileSync(path.join(__dirname+'/public/stylesheets/', 'myStyles.css'), 'utf8'));
+    mainWindow.show();
+  });
+
+  mainWindow.on('close', function() {
+    mainWindowBounds = mainWindow.getBounds();
+  });
+
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
+});
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin')
     app.quit();
+    console.log(mainWindowBounds);
 });
 
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
-app.on('ready', function() {
-
-  mainWindow = new BrowserWindow({
-    width: 1000, 
-    height: 800
-  });
-
-  // NOTE: the URL is enclosed in backticks NOT single quotes
-  mainWindow.loadURL(`file://${__dirname}/index.jade`);
-  
-  mainWindow.on('closed', function() {
-    mainWindow = null;
+app.on('before-quit', function() {
+  isQuitting = true;
+  config.set('lastWindowState', {
+    width: mainWindowBounds.width,
+    height: mainWindowBounds.height,
+    x: mainWindowBounds.x,
+    y: mainWindowBounds.y
   });
 });
--- public/js/db.js
+++ public/js/db.js
@@ -1,5 +1,6 @@
 var moment = require('moment');
-var config = require('./lib/config');
+var config = require('./lib/db-config');
+var query_interactions = require('./lib/query-interactions');
 
 var sql = require('seriate');
 sql.setDefaultConfig(config);
@@ -153,31 +154,14 @@ function build_homepage_calls_abd_flow_tran_graph(data) {
 
 // ---------------------------------------------------------
 
-var query_1 = "SELECT ED.WeekEnding AS week_ending, I.Solution AS solution, SUM(I.nEnteredAcd) AS nEnteredAcd, SUM(I.nAnsweredAcd) AS nAnsweredAcd, SUM(I.nAbandonedAcd) AS nAbandonedAcd, SUM(I.nFlowOutAcd) AS nFlowOutAcd, SUM(I.nTransferedAcd) AS nTransferedAcd FROM EmployeeServices.dbo.Agent_ININ_Data_By_Day AS I INNER JOIN EmployeeServices.dbo.EmployeeDates AS ED ON I.FullDate = ED.FullDate WHERE I.InteractionType = 'Call' GROUP BY ED.WeekEnding, I.Solution ORDER BY ED.WeekEnding, I.Solution";
-
-var query_2 = "SELECT \
-  ED.WeekEnding AS week_ending, \
-  I.Solution AS solution, \
-  SUM(I.nEnteredAcd) AS nEnteredAcd, \
-  SUM(I.nAnsweredAcd) AS nAnsweredAcd, \
-  SUM(I.nAbandonedAcd) AS nAbandonedAcd, \
-  SUM(I.nFlowOutAcd) AS nFlowOutAcd, \
-  SUM(I.nTransferedAcd) AS nTransferedAcd \
-  FROM EmployeeServices.dbo.Agent_ININ_Data_By_Day AS I \
-  INNER JOIN EmployeeServices.dbo.EmployeeDates AS ED \
-  ON I.FullDate = ED.FullDate \
-  WHERE I.InteractionType = 'Call' \
-  AND I.Solution = 'CRM' \
-  GROUP BY ED.WeekEnding, I.Solution \
-  ORDER BY ED.WeekEnding, I.Solution"
-
 window.onload = function() {
   sql.execute( {
-    query: query_1
+    query: query_interactions.call_metrics_by_solution_by_week
   }).then( function( data ) {
     console.log(data);
     build_homepage_calls_ans_vs_off_graph(data);
     build_homepage_calls_abd_flow_tran_graph(data);
+    console.log(location);
 
   }, function( err ) {
     console.log( err );
