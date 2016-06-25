64c802b1823f1c9c0a799d76009e97bce2f7aa27
Stephen Montana <stephen.a.montana@gmail.com>
2016-06-24 22:48:15 -0500

    almost done with QPA YTD reports page

--- main-process/db/queries/qpa_queries.js
+++ main-process/db/queries/qpa_queries.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-  get_QPA_solution_avg_by_yr: "EmployeeServices.dbo.get_QPA_solution_avg_by_yr '2016'"
+  get_QPA_solution_avg_by_yr: "EmployeeServices.dbo.get_QPA_solution_avg_by_yr '2016'",
+  get_QPA_all_solution_avgs_month_over_month_by_yr: "EmployeeServices.dbo.get_QPA_solution_avg_month_over_month_by_year '2016'"
 
 };
